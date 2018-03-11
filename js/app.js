92630d7c10e0606563724044300e243d6e24d768
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-03-11 19:43:57 -0300

    adds image counting

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgSrc = ko.observable('./img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/');
 
     this.incrementCounter = function() {
