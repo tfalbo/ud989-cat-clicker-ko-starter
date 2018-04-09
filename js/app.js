1fda61e3588318611e4e103b88bccd0073c32ee6
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-04-09 02:47:47 -0300

    separates model

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-var ViewModel = function() {
+var Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('./img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/');
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
+    
 
     this.level = ko.computed(function(){
-        if(this.clickCount() < 10){
+        var clicks = this.clickCount();
+        if(clicks < 10){
             return "Newborn";
         }
-        else if (this.clickCount() >= 10 && this.clickCount() < 20 ){
+        else if (clicks >= 10 && clicks < 20 ){
             return "Infant";
         }
-        else if(this.clickCount() >= 20 && this.clickCount() < 30){
+        else if(clicks >= 20 && clicks < 30){
             return "Teen";
         }
         else {
@@ -24,6 +23,17 @@ var ViewModel = function() {
     }, this);
 
     this.nicknames = ko.observableArray(['TabTab', 'T-Bone' ,'Mister T']);
+}
+
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+    
 }
 
 ko.applyBindings(new ViewModel());
