b67ede21ec4060e0e3b89157676498e4571739ca
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-04-09 13:19:15 -0300

    adds more cats

--- js/app.js
+++ js/app.js
@@ -1,37 +1,88 @@
-var Cat = function(){
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('./img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/');
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+    }  
+]
 
-    this.level = ko.computed(function(){
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+
+    this.title = ko.computed(function(){
+        var title;
         var clicks = this.clickCount();
         if(clicks < 10){
-            return "Newborn";
+            title = "Newborn";
         }
-        else if (clicks >= 10 && clicks < 20 ){
-            return "Infant";
+        else if (clicks < 50 ){
+            title = "Infant";
         }
-        else if(clicks >= 20 && clicks < 30){
-            return "Teen";
+        else if (clicks < 100 ){
+            title = "Infant";
+        }
+        else if(clicks < 200){
+            title = "Teen";
+        }
+        else if(clicks < 500){
+            title =  "Adult";
         }
         else {
-            return "Adult";
+            title = "Ninja";
         }
+        return title;
     }, this);
 
-    this.nicknames = ko.observableArray(['TabTab', 'T-Bone' ,'Mister T']);
+    this.nicknames = ko.observableArray(data.nicknames);
 }
 
 
 var ViewModel = function() {
     var self = this;
 
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([])
+
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+
+    this.setCat = function(clickedCat){
+        self.currentCat(clickedCat);
+    }
     
 }
 
